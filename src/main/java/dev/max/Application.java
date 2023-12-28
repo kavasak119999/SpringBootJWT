da944071ff88e4a0735ee62baa5e0bc99e36b3ff
kavasak119999 <kavasak50@gmail.com>
2023-12-28 10:55:32 +0200

    Excluding default security password generating

--- src/main/java/dev/max/Application.java
+++ src/main/java/dev/max/Application.java
@@ -2,8 +2,9 @@ package dev.max;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
